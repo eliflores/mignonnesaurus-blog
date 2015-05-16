3479f83b03dc1795987c56baf12bdb3274fcc29d
Ely Flores <elifloresch@gmail.com>
2015-05-16 06:08:35 +0000

    My Django Girls app, first commit

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+	print('Hi ' + name + '!')
+
+girls = ['Ely', 'Abril', 'Anita', 'Meli', 'You']
+for name in girls:
+	hi(name)
+	print('Next girl')
+
+for i in range(1, 6):
+	print(i)
