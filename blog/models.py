23293f092fa5be8d3a279ec23fbbb7a9ffc4eeb3
elifloresch <elifloresch@gmail.com>
2017-06-15 22:52:18 +0200

    Upgrade to django 1.10

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-	author = models.ForeignKey('auth.User')
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(
-		default = timezone.now)
-	published_date = models.DateTimeField(
-		blank=True, null=True)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
 
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-	def approved_comments(self):
-		return self.comments.filter(approved=True)
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-	url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
