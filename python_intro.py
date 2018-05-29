a5da915686c82ddb8e1bb951c03d2d0f9eae7609
mignonnesaurus <elifloresch@gmail.com>
2018-05-29 23:26:24 +0200

    Format python_intro.py

--- python_intro.py
+++ python_intro.py
@@ -1,10 +1,11 @@
 def hi(name):
-	print('Hi ' + name + '!')
+    print('Hi ' + name + '!')
+
 
 girls = ['Ely', 'Abril', 'Anita', 'Meli', 'You']
 for name in girls:
-	hi(name)
-	print('Next girl')
+    hi(name)
+    print('Next girl')
 
 for i in range(1, 6):
-	print(i)
+    print(i)
