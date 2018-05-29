bc8fbf08ff966c9589e96643c5850cb738be9864
mignonnesaurus <elifloresch@gmail.com>
2018-05-29 21:45:29 +0200

    Format forms.py and add django in requirements.txt

--- blog/forms.py
+++ blog/forms.py
@@ -2,12 +2,14 @@ from django import forms
 
 from .models import Post, Comment
 
+
 class PostForm(forms.ModelForm):
-	class Meta:
-		model = Post
-		fields = ('title', 'text',)
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+
 
 class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ('author', 'text',)
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
