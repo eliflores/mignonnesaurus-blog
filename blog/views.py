cb8689d99ab9943c42882ef720c01701aece76de
mignonnesaurus <eli@mignonnesaurus.com>
2019-04-28 01:02:00 +0200

    Testing Hound CI

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    if False:
+        print("Hola")
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
