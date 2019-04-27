898451ef35e3189c8e7d40c1e266af52bbf8d82f
mignonnesaurus <elifloresch@gmail.com>
2019-04-27 20:03:53 +0200

    Setup static configuration for staging and dev

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -9,4 +11,4 @@ urlpatterns = [
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     url(r'', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
