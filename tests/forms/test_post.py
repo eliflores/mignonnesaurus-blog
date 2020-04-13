66adb5adf3c897b61b2fa49bd5f48507a100ef4d
mignonnesaurus <eli@mignonnesaurus.com>
2020-04-13 21:55:58 +0200

    Refactor Form Tests
    Move to a separate directoy and separate per form

---
+++ tests/forms/test_post.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from blog.forms import PostForm
+
+
+class PostFormTest(TestCase):
+    def test_is_valid_when_both_title_and_text_are_present(self):
+        form = PostForm(data={'title': 'test title', 'text': 'test text'})
+
+        self.assertTrue(form.is_valid())
