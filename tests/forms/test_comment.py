8149b74e478b13c2f66960914f863ef89f99228b
Eli Flores <eli@mignonnesaurus.com>
2020-04-13 22:16:04 +0200

    Refactor Form Tests (#157)
    * Move to a separate directoy and separate per form
    * Use TestCase from django.test instead of unittest

---
+++ tests/forms/test_comment.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from blog.forms import CommentForm
+
+
+class CommentFormTest(TestCase):
+    def test_is_valid_when_both_author_and_text_are_present(self):
+        form = CommentForm(data={'author': 'Han Solo', 'text': 'test text'})
+
+        self.assertTrue(form.is_valid())
