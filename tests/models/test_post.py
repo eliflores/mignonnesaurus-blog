ac55766ea9b791fc23c5e98a90e0c195aabd0548
mignonnesaurus <eli@mignonnesaurus.com>
2020-04-13 22:39:47 +0200

    Add a new test case for Post Model
    And some refactoring of the test.

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -8,16 +8,22 @@ User = get_user_model()
 
 class PostModelTest(TestCase):
     def setUp(self):
-        user = User.objects.create(username='testuser')
-        Post.objects.create(title='Test post', author=user)
+        self.user = User.objects.create(username='testuser')
 
-    def test_published_posts_have_a_published_date(self):
-        post = Post.objects.get(title='Test post')
+    def test_post_does_not_have_a_published_date_by_default(self):
+        post = Post.objects.create(title='Test post', author=self.user)
+
+        self.assertIsNone(post.published_date)
+
+    def test_a_publish_post_has_a_published_date(self):
+        post = Post.objects.create(title='Test post', author=self.user)
+
         post.publish()
         self.assertIsNotNone(post.published_date)
 
     @freeze_time('2019-01-01 00:01:01')
     def test_published_date_is_time_now_by_default(self):
-        post = Post.objects.get(title='Test post')
+        post = Post.objects.create(title='Test post', author=self.user)
+
         post.publish()
         self.assertEqual(post.published_date.strftime("%Y-%m-%d %H:%M:%S"), '2019-01-01 00:01:01')
