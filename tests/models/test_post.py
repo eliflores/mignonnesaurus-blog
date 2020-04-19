e10726d2a2b8800d066a96c09511986edacbb7c2
mignonnesaurus <eli@mignonnesaurus.com>
2020-04-19 22:55:56 +0200

    Introduce is_draft as a property of Post model
    Rewrite tests based on this new property.

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -10,16 +10,18 @@ class PostModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username='testuser')
 
-    def test_post_does_not_have_a_published_date_by_default(self):
+    def test_a_post_without_a_published_date_is_a_draft(self):
         post = Post.objects.create(title='Test post', author=self.user)
 
         self.assertIsNone(post.published_date)
+        self.assertTrue(post.draft)
 
-    def test_a_publish_post_has_a_published_date(self):
+    def test_once_a_post_is_published_it_is_no_longer_a_draft(self):
         post = Post.objects.create(title='Test post', author=self.user)
 
         post.publish()
         self.assertIsNotNone(post.published_date)
+        self.assertFalse(post.draft)
 
     @freeze_time('2019-01-01 00:01:01')
     def test_published_date_is_time_now_by_default(self):
