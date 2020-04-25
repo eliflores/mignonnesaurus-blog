0c9f97327ac9f6de9a6a7f49a0d51559f63c71e3
mignonnesaurus <eli@mignonnesaurus.com>
2020-04-25 19:24:52 +0200

    Write a test for approval of comments

--- tests/models/test_comment.py
+++ tests/models/test_comment.py
@@ -7,11 +7,18 @@ User = get_user_model()
 
 
 class CommentModelTest(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         user = User.objects.create(username='testuser')
         Post.objects.create(title='Test post', author=user)
 
     def test_a_comment_is_by_default_not_approved(self):
         post = Post.objects.get(title='Test post')
-        comment = Comment.objects.create(post=post)
+        comment = Comment.objects.create(post=post, author="Kenobi", text="Use the Force.")
         self.assertFalse(comment.approved)
+
+    def test_a_comment_can_be_approved(self):
+        post = Post.objects.get(title='Test post')
+        comment = Comment.objects.create(post=post, author="Vader", text="Join the Dark Side.")
+        comment.approve()
+        self.assertTrue(comment.approved)
