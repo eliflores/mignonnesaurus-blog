ad0e377ad5d8a19db4d9ac38356305107d405093
mignonnesaurus <eli@mignonnesaurus.com>
2020-04-25 19:25:11 +0200

    Remove extra line breaks in Post Test

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -12,13 +12,11 @@ class PostModelTest(TestCase):
 
     def test_a_post_without_a_published_date_is_a_draft(self):
         post = Post.objects.create(title='Test post', author=self.user)
-
         self.assertIsNone(post.published_date)
         self.assertTrue(post.draft)
 
     def test_once_a_post_is_published_it_is_no_longer_a_draft(self):
         post = Post.objects.create(title='Test post', author=self.user)
-
         post.publish()
         self.assertIsNotNone(post.published_date)
         self.assertFalse(post.draft)
@@ -26,6 +24,5 @@ class PostModelTest(TestCase):
     @freeze_time('2019-01-01 00:01:01')
     def test_published_date_is_time_now_by_default(self):
         post = Post.objects.create(title='Test post', author=self.user)
-
         post.publish()
         self.assertEqual(post.published_date.strftime("%Y-%m-%d %H:%M:%S"), '2019-01-01 00:01:01')
