a224041ce86f36ff5aea9ccae60d362a86a3571c
mignonnesaurus <eli@mignonnesaurus.com>
2020-08-21 23:03:57 +0200

    Add Headless E2E Tests
    This will enable automated testing of changes, such as Django major upgrades, that could break main functionality.

---
+++ e2e/headless/test_post_create.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox import options
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from e2e.driver import SeleniumDriver
+from e2e.pages.admin import AdminLoginPage
+from e2e.pages.drafts import DraftsPage
+from e2e.pages.home import HomePage
+from e2e.pages.new_post import NewPostPage
+
+
+class PostCreateTestCase(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        firefox_options = options.Options()
+        firefox_options.headless = True
+        cls.webdriver = WebDriver(options=firefox_options)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.webdriver.quit()
+
+    def setUp(self):
+        self.page_driver = SeleniumDriver(self.webdriver)
+        get_user_model().objects.create_superuser('admin', 'admin@example.com', 'password')
+
+    def test_a_post_can_be_created(self):
+        home_page = HomePage(self.page_driver, self.live_server_url)
+        new_post_page = NewPostPage(self.page_driver)
+        drafts_page = DraftsPage(self.page_driver, self.live_server_url)
+
+        self.login()
+
+        home_page.load()
+        home_page.new_post()
+
+        post_title = 'Post Title'
+        new_post_page.add_post(post_title, 'Post content')
+
+        self.assertTrue(drafts_page.has_post_with_title(post_title))
+
+        home_page.logout()
+
+    def login(self):
+        admin_login_page = AdminLoginPage(self.page_driver, self.live_server_url)
+        admin_login_page.load()
+        admin_login_page.login("admin", "password")
---
+++ e2e/headless/test_post_publish.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox import options
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from e2e.driver import SeleniumDriver
+from e2e.pages.admin import AdminLoginPage
+from e2e.pages.home import HomePage
+from e2e.pages.new_post import NewPostPage
+
+
+class PostPublishTestCase(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        firefox_options = options.Options()
+        firefox_options.headless = True
+        cls.webdriver = WebDriver(options=firefox_options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.webdriver.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.page_driver = SeleniumDriver(self.webdriver)
+        get_user_model().objects.create_superuser('admin', 'admin@example.com', 'password')
+
+    def test_a_post_can_be_published(self):
+        home_page = HomePage(self.page_driver, self.live_server_url)
+        new_post_page = NewPostPage(self.page_driver)
+
+        self.login()
+
+        home_page.load()
+        home_page.new_post()
+
+        post_title = 'Post Title'
+        new_post_page.add_post(post_title, 'Post content')
+        new_post_page.publish_post()
+
+        home_page.load()
+        self.assertTrue(home_page.has_post_with_title(post_title))
+
+        home_page.logout()
+
+    def login(self):
+        admin_login_page = AdminLoginPage(self.page_driver, self.live_server_url)
+        admin_login_page.load()
+        admin_login_page.login("admin", "password")
